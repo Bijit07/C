983b0bacc1197dd60be2fd108c5bb666b1dc5593
Bijit07 <bijitshah@gmail.com>
2016-11-14 20:47:35 -0800

    Add files via upload

---
+++ printname.c
@@ -0,0 +1,19 @@
+/*
+Student Name: Bijit Bikram Shah
+Subject: Programming Fundamental
+Roll no :
+Lab No:
+Program: WAP to print your name 10 times
+Date: 15th November,2016
+/*
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   for(inti=0;i<10;i++)
+   {
+   printf("\nBijit);
+   }
+   return0;
+}
